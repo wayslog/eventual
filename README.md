c301bfc13471cfed855b3abf99bb8bd666c151e9
wayslog <wayslog@163.com>
2015-11-02 13:14:24 +0800

    add doc for lib.rs

--- README.md
+++ README.md
@@ -21,3 +21,11 @@ Then, add this to your crate root:
 ```rust
 extern crate eventual;
 ```
+
+## About-doc-zh-cn
+
+关于中文文档的一些说明：
+
+1. 本文力图还原作者的话，但是涉及翻译能力有限，主业还是程序员，所以有纰漏的地方欢迎大家指正。
+2. 关于`译者读代码注`：这个是我[@wayslog](http://github.com/wayslog)的个人读源码的一些解释，涉及到的多半是作者还没有加注释的地方，仅供参考
+3. 欢迎和我一起维护中文文档
--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,34 @@
-//! Composable primitives for asynchronous computations
+//! 异步计算的基本组件
 //!
-//! The async module contains utilities for managing asynchronous computations.
-//! These utilities are primarily based around `Future` and `Stream` types as
-//! well as functions that allow composing computations on these types.
+//! 本模块包含一系列用于管理异步计算的工具
+//! 这些工具大部分是基于`Future`和`Stream`这两个类型实现的。
+//! 同时，这些类型的函数，也允许我们对计算进行合成。
+//! （译者注：本文中计算原义为computation，才疏学浅，不知有什么更好的翻译，各位海涵。）
 //!
 //! ## Future
 //!
-//! A `Future` is a proxy representing the result of a computation which may
-//! not be complete.  The computation may be running concurrently in another
-//! thread or may be triggered upon completion of an asynchronous callback. One
-//! way to think of a `Future` is as a `Result` where the value is
-//! asynchronously computed.
+//! `Future`是一个对计算结果的代理，这些计算可能是暂时未完成的。
+//! 这些计算可能会被并行的跑在另一个线程里或者当计算完成时可以触发其异步回调函数。
+//! 一种理解就是`Future`就是一个异步计算的结果，其值就是异步计算结果的值。
+//! （译者注：作者是不是rust写多了，写文档的时候还带着unwrap……）
 //!
 //! For example:
 //!
 //! ```
 //! use eventual::*;
 //!
-//! // Run a computation in another thread
+//! // 在一个线程里跑一个计算
 //! let future1 = Future::spawn(|| {
-//!     // Represents an expensive computation, but for now just return a
-//!     // number
+//!     // 一个巨复杂无比的计算，当然，这里我们就小小的返回个数字
 //!     42
 //! });
 //!
-//! // Run another computation
+//! // 在另一个线程里跑计算
 //! let future2 = Future::spawn(|| {
-//!     // Another expensive computation
+//!     // 另一个巨复杂无比的计算
 //!     18
 //! });
-//!
+//! // 注意了，这里他们要合并了
 //! let res = join((
 //!         future1.map(|v| v * 2),
 //!         future2.map(|v| v + 5)))
@@ -42,8 +41,8 @@
 //!
 //! ## Stream
 //!
-//! A `Stream` is like a `Future`, except that instead of representing a single
-//! value, it represents a sequence of values.
+//! `Stream` 和`Future`差不多, 但是`Future`表达的是单独的一个计算的结果值，而`Stream`
+//! 表达的是一个结果序列。
 //!
 
 #![crate_name = "eventual"]
@@ -88,21 +87,28 @@ mod stream;
 mod timer;
 
 /// A value representing an asynchronous computation
+/// 每一个结果即代表一个异步计算
 pub trait Async : Send + 'static + Sized {
+    /// 译者读代码注：
+    /// `Value`最后计算的结果类型
     type Value: Send + 'static;
+    /// 译者读代码注：
+    /// `Error`是这个计算过程中出错而抛出的自定义类型
     type Error: Send + 'static;
+    /// 译者读代码注：
+    /// `Cancel`顾名思义是一个用来取消计算结果的类型
     type Cancel: Cancel<Self>;
 
-    /// Returns true if `expect` will succeed.
+    /// 如果计算能成功则返回 true
     fn is_ready(&self) -> bool;
 
-    /// Returns true if the async value is ready and has failed
+    /// 与`is_ready`对应，如果计算完成并且计算失败的时候则返回 true
     fn is_err(&self) -> bool;
 
-    /// Get the underlying value if present
+    /// 如果底层有计算结果出现，无论成功或失败完成未完成，获得之
     fn poll(self) -> Result<AsyncResult<Self::Value, Self::Error>, Self>;
 
-    /// Get the underlying value if present, panic otherwise
+    /// 如果底层有计算结果出现，无论成功失败，获得之。如果计算还未完成，则会引发panic.
     fn expect(self) -> AsyncResult<Self::Value, Self::Error> {
         if let Ok(v) = self.poll() {
             return v;
@@ -111,11 +117,10 @@ pub trait Async : Send + 'static + Sized {
         panic!("the async value is not ready");
     }
 
-    /// Invokes the given function when the Async instance is ready to be
-    /// consumed.
+    /// 如果计算结果能被消费了，则执行回调函数
     fn ready<F>(self, f: F) -> Self::Cancel where F: FnOnce(Self) + Send + 'static;
 
-    /// Invoke the callback with the resolved `Async` result.
+    /// 如果`AsyncResult`已经准备好了，调用回调函数来接收`AsyncResult`
     fn receive<F>(self, f: F)
             where F: FnOnce(AsyncResult<Self::Value, Self::Error>) + Send + 'static {
         self.ready(move |async| {
@@ -126,8 +131,7 @@ pub trait Async : Send + 'static + Sized {
         });
     }
 
-    /// Blocks the thread until the async value is complete and returns the
-    /// result.
+    /// 阻塞当前线程直到`AsyncResult`被计算完成并且返回了结果
     fn await(self) -> AsyncResult<Self::Value, Self::Error> {
         use std::sync::mpsc::channel;
 
@@ -137,7 +141,7 @@ pub trait Async : Send + 'static + Sized {
         rx.recv().ok().expect("async disappeared without a trace")
     }
 
-    /// Trigger the computation without waiting for the result
+    /// 触发计算，但是并不接收其计算结果
     fn fire(self) {
         self.receive(drop)
     }
@@ -148,38 +152,59 @@ pub trait Async : Send + 'static + Sized {
      *
      */
 
-    /// This method returns a future whose completion value depends on the
-    /// completion value of the original future.
+    /// 本method返回一个`Future`实例，结果依赖于原本的`Future`
+    /// (译者注：可以认为就是调用者的self)的计算结果
     ///
-    /// If the original future completes with an error, the future returned by
-    /// this method completes with that error.
+    /// 如果原`Future`返回的结果是Err，则本method返回的`Future`的计算结果将是这个Error。
     ///
-    /// If the original future completes successfully, the future returned by
-    /// this method completes with the completion value of `next`.
+    /// 如果原`Future`返回的是Ok，那么由本method返回的`Future`的计算结果将是其原本的结果值
+    ///
+    /// 译者读代码注：
+    ///
+    /// 这里可以理解为指挥官-士兵的模型——
+    /// 指挥官大脑里打了很多小算盘来算计是不是要排出士兵去攻击敌方阵地。
+    ///
+    /// 当指挥官觉得不行，这样做风险太大的时候，他抛出了一个错误(Err)，于是士兵们知道了指挥官不要他们出击，
+    /// 但是士兵不会思考，他只会机械的将指挥官的错误告诉别人。
+    ///
+    /// 当指挥官觉得可行的时候，他会告诉士兵，你们可以去攻击了，由于士兵们都被提前设定好了攻击步骤，
+    /// 于是士兵们得到命令就高高兴兴的出发了，战斗的结果的话，还是要看士兵的战斗力了哈。
     fn and<U: Async<Error=Self::Error>>(self, next: U) -> Future<U::Value, Self::Error> {
         self.and_then(move |_| next)
     }
 
-    /// This method returns a future whose completion value depends on the
-    /// completion value of the original future.
+    /// 本method返回一个`Future`实例，计算结果依赖于原本的`Future`
     ///
-    /// If the original future completes with an error, the future returned by
-    /// this method completes with that error.
+    /// 如果原`Future`返回的结果是Err，则本method返回的`Future`的计算结果将是这个Error。
     ///
-    /// If the original future completes successfully, the callback to this
-    /// method is called with the value, and the callback returns a new future.
-    /// The future returned by this method then completes with the completion
-    /// value of that returned future.
+    /// 如果原`Future`返回的是Ok，那么参数中的callback函数将会接收到这个值并且被执行，
+    /// 并且这个callback函数将会返回一个异步结果(Async)
+    /// 最后，本method返回的`Future`将会返回callback函数的计算结果。
+    ///
+    /// 译者读代码注：
+    ///
+    /// 好吧，我觉得还是有人不懂，那么在用一个模型来表达就是指挥官-队长-士兵的模型——
+    ///
+    /// 接着上一个模型来看，当指挥官的命令下达之后，队长将分析指挥官的命令，然后队长也经历了一个小波头脑风暴，
+    ///
+    /// 队长觉得可以，就上，最后的结果算我的。
+    ///
+    /// 队长觉得不可以，将在外军令有所不受，把指挥官的命令当擦屁股纸了。
+    ///
+    /// 士兵们表示我们是无辜的，上面说啥就是啥
     ///
     /// ```
     /// use eventual::*;
     ///
     /// let f = Future::of(1337);
     ///
-    /// f.and_then(|v| {
+    /// f.and_then(|v|
     ///     assert_eq!(v, 1337);
+    ///     // 返回了一个正确的计算结果 Ok(1007)
     ///     Ok(1007)
     /// }).and_then(|v| {
+    ///     // assert_eq!() 将返回一个unit_type,其值为()
+    ///     // 至于why，看下面 `impl Async for ()`
     ///     assert_eq!(v, 1007)
     /// }).await();
     ///
@@ -218,30 +243,17 @@ pub trait Async : Send + 'static + Sized {
         ret
     }
 
-    /// This method returns a future whose completion value depends on the
-    /// completion value of the original future.
-    ///
-    /// If the original future completes successfully, the future returned by
-    /// this method will complete with that value.
-    ///
-    /// If the original future completes with an error, the future returned by
-    /// this method will complete with the completion value of the `alt` future
-    /// passed in. That can be either a success or error.
+    /// 本method返回一个`Future`实例，计算结果依赖于原本的`Future`
+    /// 这个函数，简单来说就是`and`的反向逻辑，原函数给的Ok的结果，我接受并且返回，
+    /// 原函数给的错误的结果，那么我就用我自己的`Future`去替代它。
     fn or<A>(self, alt: A) -> Future<Self::Value, A::Error>
             where A: Async<Value=Self::Value> {
         self.or_else(move |_| alt)
     }
 
-    /// This method returns a future whose completion value depends on the
-    /// completion value of the original future.
-    ///
-    /// If the original future completes successfully, the future returned by
-    /// this method will complete with that value.
-    ///
-    /// If the original future completes with an error, this method will invoke
-    /// the callback passed to the method, which should return a future. The
-    /// future returned by this method will complete with the completion value
-    /// of that future. That can be either a success or error.
+    /// 本method返回一个`Future`实例，计算结果依赖于原本的`Future`
+    /// 与`and_else`相反，这个函数在原`Future`返回一个Err的时候时候去调用callback函数，
+    /// 处理并返回新结果。
     fn or_else<F, A>(self, f: F) -> Future<Self::Value, A::Error>
             where F: FnOnce(Self::Error) -> A + Send + 'static,
                   A: Async<Value=Self::Value> {
